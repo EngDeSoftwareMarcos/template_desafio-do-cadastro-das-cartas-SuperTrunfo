afdf8263f5466d55cab9caa50d7f7da8588a3fc7
EngDeSoftwareMarcos <marcosvitorcosta123@gmail.com>
2026-02-26 01:25:00 +0000

    Adicionei as variaveis e as perguntas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,30 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char carta;
+char estado;
+char Código[50];
+char Nome_da_Cidade [50];
+int População;
+float Área;
+float PIB;
+int Número_de_Pontos_Turísticos; 
 
   // Área para entrada de dados
+printf("Qual sua carta?\n");
+scanf("%c",&carta);
 
+printf("Estado:\n");
+scanf("%c", estado);
+
+printf("Código:\n");
+scanf("%s", &Código);
+
+printf("Nome_da_Cidade:\n");
+scanf("%s", &Nome_da_Cidade);
+
+printf("População:\n");
+scanf("%d", &População);
   // Área para exibição dos dados da cidade
 
 return 0;
